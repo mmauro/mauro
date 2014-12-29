29b79895d2a984421a16072d5ddc0b11ad83e55b
mmauro <subzero.mauro@gmail.com>
2014-12-29 11:02:38 +0100

    aggiunti campi mancanti BookModel

--- VideoBookApplication/VideoBookApplication/library/model/database/BookModel.cs
+++ VideoBookApplication/VideoBookApplication/library/model/database/BookModel.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace VideoBookApplication.library.model.database
 {
-    class BookModel
+    public class BookModel
     {
         public int idLibro { get; set; }
         public string titolo { get; set; }
+        public string serie { get; set; }
+        public bool flEbook { get; set; }
         public AuthorModel autore { get; set; }
         public CategoryModel category { get; set; }
         public PositionModel position { get; set; }
