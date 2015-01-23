4c072af2f955c404156fe9e39b6f38ce1c432461
mmauro <subzero.mauro@gmail.com>
2015-01-23 22:37:21 +0100

    Aggiunto riferimento a libreria ZedChart Modificato Layout pannello grafici

--- VideoBookApplication/VideoBookApplication/library/view/StatGraphPanel.Designer.cs
+++ VideoBookApplication/VideoBookApplication/library/view/StatGraphPanel.Designer.cs
@@ -28,16 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grafico Categorie";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(8, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Numero Libri / Categorie";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(16, 16);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Distribuzione Libri";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.BackgroundImage = global::VideoBookApplication.Properties.Resources.button_ok;
+            this.buttonOk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonOk.Location = new System.Drawing.Point(254, 136);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(48, 48);
+            this.buttonOk.TabIndex = 1;
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
             // 
             // StatGraphPanel
             // 
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Size = new System.Drawing.Size(800, 500);
+            this.Size = new System.Drawing.Size(900, 350);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonOk;
     }
 }
--- VideoBookApplication/VideoBookApplication/library/view/StatGraphPanel.cs
+++ VideoBookApplication/VideoBookApplication/library/view/StatGraphPanel.cs
@@ -47,6 +47,45 @@ namespace VideoBookApplication.library.view
             TitlePanel titlePanel = new TitlePanel("Grafici Statistiche", this);
             titlePanel.Location = new Point(0, 0);
             this.Controls.Add(titlePanel);
+
+            groupBox1.Size = new Size((this.Size.Width / 3) - 10, this.Size.Height - 150);
+            groupBox1.Location = new Point(5, titlePanel.Location.Y + titlePanel.Size.Height + 5);
+            groupBox1.BackColor = LayoutManager.getPanelColor2();
+            this.Controls.Add(groupBox1);
+
+            groupBox2.Size = new Size((this.Size.Width / 3) - 10, this.Size.Height - 150);
+            groupBox2.Location = new Point(groupBox1.Location.X + 10 + groupBox1.Size.Width, groupBox1.Location.Y);
+            groupBox2.BackColor = LayoutManager.getPanelColor2();
+            this.Controls.Add(groupBox2);
+
+            groupBox3.Size = new Size((this.Size.Width / 3) - 10, this.Size.Height - 150);
+            groupBox3.Location = new Point(groupBox2.Location.X + 10 + groupBox2.Size.Width, groupBox2.Location.Y);
+            groupBox3.BackColor = LayoutManager.getPanelColor2();
+            this.Controls.Add(groupBox3);
+
+            //Gestione pulsanti
+            buttonOk.Location = new Point(this.Size.Width - (20 + buttonOk.Size.Height), groupBox3.Location.Y + groupBox3.Size.Height + 10);
+            this.Controls.Add(buttonOk);
+
+            if (globalObject.libraryObject.statistiche.categoryDistribution != null && globalObject.libraryObject.statistiche.categoryDistribution.Count > 0)
+            {
+                
+            }
+
+            //Disegno pie chart con numero libri carta / numero libri ebook
+            if (globalObject.libraryObject.statistiche.numLibri > 0)
+            {
+            }
+
+        }
+
+        private void displayPieChartBooksNumber()
+        {
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            parent.closePanel(GlobalOperation.LIB_STATS_GRAPH);
         }
     }
 }
