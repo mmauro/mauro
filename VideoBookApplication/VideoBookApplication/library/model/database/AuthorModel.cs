13b2cd40a2c64a33db517aa05c4f09e0f20b981d
mmauro <subzero.mauro@gmail.com>
2015-01-29 21:56:09 +0100

    tolto log

--- VideoBookApplication/VideoBookApplication/library/model/database/AuthorModel.cs
+++ VideoBookApplication/VideoBookApplication/library/model/database/AuthorModel.cs
@@ -8,9 +8,6 @@ namespace VideoBookApplication.library.model.database
 {
     public class AuthorModel
     {
-
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         public int idAutore { get; set; }
         public string cognome {get; set;}
         public string nome { get; set; }
@@ -27,24 +24,10 @@ namespace VideoBookApplication.library.model.database
             {
                 String value = cognome + nome;
                 hash = value.GetHashCode();
-                //return value.GetHashCode();
             }
 
-            log.Info("HC - " + hash.ToString());
             return hash;
-
         }
 
-        public bool Equals(AuthorModel other)
-        {
-            if (idAutore > 0 && other.idAutore > 0)
-            {
-                return (idAutore == other.idAutore);
-            }
-            else
-            {
-                return (cognome.Equals(other.cognome) && nome.Equals(other.nome));
-            }
-        }
     }
 }
