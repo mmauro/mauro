475f421d65abaf197012f25ac86d25bc3dd43977
mmauro <subzero.mauro@gmail.com>
2015-03-01 11:42:35 +0100

    Inizio scrittura metodi per CSV

---
+++ VideoBookApplication/VideoBookApplication/customCSV/common/FormatterCSV.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoBookApplication.customCSV.common
+{
+    public class FormatterCSV
+    {
+        public string cellSeparator { get; private set; }
+        public string rowSeparator { get; private set; }
+        public string cellQuote { get; private set; }
+
+        private FormatterCSV(string cellSeparator, string cellQuote, string rowSeparator)
+        {
+            this.cellQuote = cellQuote;
+            this.rowSeparator = rowSeparator;
+            this.cellSeparator = cellSeparator;
+        }
+
+        public static FormatterCSV DEFAULT_FORMATTER = new FormatterCSV(";", "\"", Environment.NewLine);
+    }
+}
---
+++ VideoBookApplication/VideoBookApplication/customCSV/operations/WriteCSV.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoBookApplication.customCSV.common;
+
+namespace VideoBookApplication.customCSV.operations
+{
+    public class WriteCSV
+    {
+
+        private FormatterCSV formatter = null;
+        public WriteCSV()
+        {
+            formatter = FormatterCSV.DEFAULT_FORMATTER;
+        }
+    }
+}
