cf3fe16be79bb2cf3b3f8a35beb93c856e2f4d23
mmauro <subzero.mauro@gmail.com>
2015-03-09 16:42:09 +0100

    Aggiunto salvataggio nome file Gestione corretta titolo finestra

--- VideoBookApplication/VideoBookApplication/common/view/SaveDisplayDialog.cs
+++ VideoBookApplication/VideoBookApplication/common/view/SaveDisplayDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,13 +27,43 @@ namespace VideoBookApplication.common.view
             {
                 SaveFileDialog saveFileDialog1 = new SaveFileDialog();
                 saveFileDialog1.Filter = this.globalObject.fileOperation.filter.dialogFilter;
+                saveFileDialog1.Title = getTitleByFilter();
                 saveFileDialog1.Title = "Salva Report";
                 saveFileDialog1.ShowDialog();
+
+
+                //Scelta del nome File
+                if (saveFileDialog1.FileName != null && !saveFileDialog1.FileName.Equals(""))
+                {
+                    log.Info("filename Selezionato = " + saveFileDialog1.FileName);
+                    globalObject.fileOperation.fileName = saveFileDialog1.FileName;
+                }
+
             }
             else
             {
                 DisplayManager.displayError(ApplicationErrorType.INVALID_VALUE);
             }
+        }
+
+        private String getTitleByFilter()
+        {
+            if (globalObject.fileOperation.filter == FileFilterType.CSV_FILE)
+            {
+                return "Salvataggio File CSV";
+            }
+            if (globalObject.fileOperation.filter == FileFilterType.EXCEL_FILE)
+            {
+                return "Salvataggio File Microsoft Excel";
+            }
+            if (globalObject.fileOperation.filter == FileFilterType.WORD_FILE)
+            {
+                return "Salvataggio File Microsoft Word";
+            }
+
+            //Di qui non dovrebbe passare
+            return "";
+
         }
 
     }
